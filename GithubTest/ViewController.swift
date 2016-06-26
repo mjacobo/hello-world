b48868481a9fb9dd47e64c13138561cf9dcea54f
Mauricio Jacobo Romero <leyenda1@mauricios-macbook-pro.local>
2016-06-25 22:03:09 -0500

    Initial Commit

---
+++ GithubTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubTest
+//
+//  Created by Mauricio Jacobo Romero on 25/06/2016.
+//  Copyright © 2016 Mauricio Jacobo Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
