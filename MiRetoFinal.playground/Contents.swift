f9632bf514313b14f60dd5145f5765fa0d2fe592
Mauricio Jacobo Romero <leyenda1@mauricios-macbook-pro.local>
2016-06-26 19:15:29 -0500

    Original load

---
+++ MiRetoFinal.playground/Contents.swift
@@ -0,0 +1,22 @@
+//Mauricio Jacobo
+//Minireto 1
+
+
+import UIKit
+var pivote = true                              //Variable pivote para validaciones
+
+for i in 0...100 {                            //Se inicializa el ciclo for para generar los nùmeros
+    //Del 0 al 100
+    switch pivote
+    {
+    case (i >= 30  && i <= 40) :              //Si el número se encuentra dentro de un rango del 30 al 40, imprime: # el número +  “Viva Swift!!!”
+        print("\(i) Viva Swift!!!")
+    case (i % 5) == 0 && i != 0:             //Si el número es divisible entre 5, imprime: # el número  + “Bingo!!!”
+        print("\(i) Bingo!!!")
+    case (i % 2) == 0 :                      //Si el número es par, imprime: # el número + “par!!!”
+        print("\(i) Par!!!")
+    default:                                 //Si el número es impar, imprime: # el número + “impar!!!”
+        print("\(i) Impar!!!")
+    }
+}
+
