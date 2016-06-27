6b62557c0dbbd7929316adf092057475f290ee53
mjacobo <mauricio.jacobor@gmail.com>
2016-06-26 19:30:02 -0500

    Update Contents.swift

--- MiRetoFinal.playground/Contents.swift
+++ MiRetoFinal.playground/Contents.swift
@@ -10,13 +10,13 @@ for i in 0...100 {                            //Se inicializa el ciclo for para 
     switch pivote
     {
     case (i >= 30  && i <= 40) :              //Si el número se encuentra dentro de un rango del 30 al 40, imprime: # el número +  “Viva Swift!!!”
-        print("\(i) Viva Swift!!!")
+        print("#\(i) Viva Swift!!!")
     case (i % 5) == 0 && i != 0:             //Si el número es divisible entre 5, imprime: # el número  + “Bingo!!!”
-        print("\(i) Bingo!!!")
+        print("#\(i) Bingo!!!")
     case (i % 2) == 0 :                      //Si el número es par, imprime: # el número + “par!!!”
-        print("\(i) Par!!!")
+        print("#\(i) Par!!!")
     default:                                 //Si el número es impar, imprime: # el número + “impar!!!”
-        print("\(i) Impar!!!")
+        print("#\(i) Impar!!!")
     }
 }
 
